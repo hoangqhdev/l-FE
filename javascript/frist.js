e465b7c39acfdb2de3253ddb4aa655ae4319abc6
SunsetSoon <hoangtran13py@gmail.com>
2023-04-21 15:32:54 +0700

    upload code

---
+++ javascript/frist.js
@@ -0,0 +1,19 @@
+/**
+ * Một số hàm built-in
+ * Alert (Hộp thoại)
+ * Console
+ * Confirm (Hộp thoại)
+ * prompt (Hộp thoại)
+ * setTimeout
+ * setInterval
+ */
+let a = 1;
+console.log(a++);
+console.log(a);
+console.log(a++);
+console.log(a);
+
+console.log(++a);
+console.log(a);
+console.log(++a);
+console.log(a);
